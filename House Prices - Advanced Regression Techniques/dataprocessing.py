6facdae910e2390b5fd5ab4c99063078aed247f1
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-10-08 22:58:55 +0900

    Error Solved & Split Main Code to Module Unit
    Error Solved : GridSearchCV Ascii problem

--- House Prices - Advanced Regression Techniques/dataprocessing.py
+++ House Prices - Advanced Regression Techniques/dataprocessing.py
@@ -1,16 +1,52 @@
-from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+
 
 def DataProcess(train, test):
-    MSSubClass_mapping = {20: 1, 30: 2, 40: 3,
-                        45: 4, 50: 5, 60: 6, 70: 7, 75: 8,
-                        80: 9, 85: 10, 90: 11, 120: 12, 150: 13,
-                        160: 14, 180: 15, 190: 16}
-    train['MSSubClass'] = train['MSSubClass'].map(MSSubClass_mapping)
-    test['MSSubClass'] = test['MSSubClass'].map(MSSubClass_mapping)
+    train.set_index('Id', inplace=True)
+    test.set_index('Id', inplace=True)
 
-    MSZoning_mapping = {"A": 1, "C": 2, "FV": 3,
-                     "I": 4, "RH": 5, "RL": 6, "RP": 7, "RM": 8}
-    train['MSZoning'] = train['MSZoning'].map(MSZoning_mapping)
-    test['MSZoning'] = test['MSZoning'].map(MSZoning_mapping)
+    train_y_label = train['SalePrice']
+    train.drop(['SalePrice'], axis=1, inplace=True)
+
+    boston_df = pd.concat((train, test), axis=0)
+    boston_df_index = boston_df.index
+
+    # check null
+    check_null = boston_df.isna().sum() / len(boston_df)
+
+    # columns of null ratio >= 0.5
+    remove_cols = check_null[check_null >= 0.5].keys()
+    boston_df = boston_df.drop(remove_cols, axis=1)
+
+    # split object & numeric
+    boston_obj_df = boston_df.select_dtypes(include='object')
+    boston_num_df = boston_df.select_dtypes(exclude='object')
+
+    boston_dummy_df = pd.get_dummies(boston_obj_df, drop_first=True)
+    boston_dummy_df.index = boston_df_index
+
+    imputer = SimpleImputer(strategy='mean')
+    imputer.fit(boston_num_df)
+    boston_num_df_ = imputer.transform(boston_num_df)
+    boston_num_df = pd.DataFrame(boston_num_df_, columns=boston_num_df.columns, index=boston_df_index)
+
+    boston_df = pd.merge(boston_dummy_df, boston_num_df, left_index=True, right_index=True)
+
+    train = boston_df[:len(train)]
+    test = boston_df[len(train):]
+
+    train['SalePrice'] = train_y_label
 
     return train, test
+
+
+def SplitTrainValid(train):
+    x_train = train.drop(['SalePrice'], axis=1)
+    y_train = train['SalePrice']
+
+    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, shuffle=True)
+
+    return x_train, x_val, y_train, y_val
--- House Prices - Advanced Regression Techniques/main.py
+++ House Prices - Advanced Regression Techniques/main.py
@@ -1,89 +1,37 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+import xgboost as xgb
 
 from dataprocessing import DataProcess
-from sklearn.impute import SimpleImputer
+from dataprocessing import SplitTrainValid
+from sklearn.model_selection import GridSearchCV
 
-
-from models import MLP
-
+pd.set_option('mode.chained_assignment',  None)
 
 if __name__ == "__main__":
     data_path = "D:/Kaggle/House Prices - Advanced Regression Techniques/"
-    train = pd.read_csv(data_path + "train.csv")
-    test = pd.read_csv(data_path + "test.csv")
+    train_csv = pd.read_csv(data_path + "train.csv")
+    test_csv = pd.read_csv(data_path + "test.csv")
 
-    train.set_index('Id', inplace=True)
-    test.set_index('Id', inplace=True)
+    train, test = DataProcess(train_csv, test_csv)
 
-    train_y_label = train['SalePrice']
-    train.drop(['SalePrice'], axis=1, inplace=True)
+    x_train, x_val, y_train, y_val = SplitTrainValid(train)
 
-    boston_df = pd.concat((train, test), axis=0)
-    boston_df_index = boston_df.index
-
-    # check null
-    check_null = boston_df.isna().sum() / len(boston_df)
-    # columns of null ratio >= 0.5
-
-    remove_cols = check_null[check_null >= 0.5].keys()
-    boston_df = boston_df.drop(remove_cols, axis=1)
-
-    # split object & numeric
-    boston_obj_df = boston_df.select_dtypes(include='object')
-    boston_num_df = boston_df.select_dtypes(exclude='object')
-
-    boston_dummy_df = pd.get_dummies(boston_obj_df, drop_first=True)
-    boston_dummy_df.index = boston_df_index
-
-    imputer = SimpleImputer(strategy='mean')
-    imputer.fit(boston_num_df)
-    boston_num_df_ = imputer.transform(boston_num_df)
-    boston_num_df = pd.DataFrame(boston_num_df_, columns=boston_num_df.columns, index=boston_df_index)
-
-    boston_df = pd.merge(boston_dummy_df, boston_num_df, left_index=True, right_index=True)
-
-    train = boston_df[:len(train)]
-    test = boston_df[len(train):]
-
-    train['SalePrice'] = train_y_label
-
-    from sklearn.model_selection import train_test_split
-
-    X_train = train.drop(['SalePrice'], axis=1)
-    y_train = train['SalePrice']
-
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, shuffle=True)
-
-    X_test = test
-    test_id_idx = test.index
-
-    from sklearn.model_selection import GridSearchCV
-    import xgboost as xgb
-
+    model = xgb.XGBRegressor()
     param = {
         'max_depth': [2, 3, 4],
-        'n_estimators': range(550, 700, 50),
+        'n_estimators': [550, 600, 650],
         'colsample_bytree': [0.5, 0.7, 1],
         'colsample_bylevel': [0.5, 0.7, 1],
     }
-    model = xgb.XGBRegressor()
     grid_search = GridSearchCV(estimator=model, param_grid=param, cv=5,
-                               scoring='neg_mean_squared_error',
-                               n_jobs=-1)
-
-    grid_search.fit(X_train, y_train)
+                               scoring='neg_mean_squared_error')
+    grid_search.fit(x_train, y_train)
     print(grid_search.best_params_)
     print(grid_search.best_estimator_)
 
-    from sklearn.metrics import mean_squared_error, mean_absolute_error
-    # pred_train = grid_search.predict(X_train)
-    # pred_val = grid_search.predict(X_val)
+    test_pred = grid_search.predict(test)
 
-    test_y_pred = grid_search.predict(X_test)
-    id_pred_df = pd.DataFrame()
-    id_pred_df['Id'] = test_id_idx
-    id_pred_df['SalePrice'] = test_y_pred
-    id_pred_df.to_csv('submission.csv', index=False)
+    submission = pd.DataFrame()
+    submission['Id'] = [i for i in range(1461, 2920)]
+    submission['SalePrice'] = test_pred
+    submission.to_csv('submission.csv', index=False)
